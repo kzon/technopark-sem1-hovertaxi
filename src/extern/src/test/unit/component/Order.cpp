b71a227436914cf049dbd2ab210f3e8c01989e87
anton <anton.cobzew@yandex.ru>
2019-03-24 15:10:50 +0300

    Remove order component (will be in PR)

--- src/extern/src/test/unit/component/Order.cpp
+++
@@ -1,9 +0,0 @@
-#include "components/order/Order.h"
-
-TEST(CreateOrderTest, OrderCreated) {
-  models::GeoPoint from{58.234234, 32.3241342}, to{58.234144, 32.3241500};
-  auto created_order = components::Order::CreateOrder(from, to);
-  ASSERT_TRUE(created_order.from == from);
-  ASSERT_TRUE(created_order.to == to);
-  ASSERT_NE(created_order.assigned_vehicle, nullptr);
-}
