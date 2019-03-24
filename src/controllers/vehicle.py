aa17678ed00d180b3976d655a7901fcb01a686cb
anton <anton.cobzew@yandex.ru>
2019-03-24 17:37:40 +0300

    Fix

--- src/controllers/vehicle.py
+++
@@ -1,8 +0,0 @@
-import tornado.web
-
-from extern import hovertaxi_extern
-
-class DemoController:
-    class IndexHandler(tornado.web.RequestHandler):
-        def get(self):
-            self.write(hovertaxi_extern.demo())
