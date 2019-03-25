bd69a2216c3feee6eaaa65e03a1f37439411ed7f
Alexandra Kruchinina <krulex98@gmail.com>
2019-03-25 16:39:43 +0300

    change Demo for managing all orders at the map

--- src/extern/src/components/demo/Demo.h
+++ src/extern/src/components/demo/Demo.h
@@ -3,14 +3,15 @@
 #include <vector>
 
 #include "models/State.h"
-#include "models/GeoPoint.h"
+#include "models/GeoPoint.cpp"
+#include "models/Order.cpp"
 #include "db/DbManager.h"
 
 namespace components {
 
 	class Demo {
 	public:	
-		Demo(models::GeoPoint _from, models::GeoPoint _to): from(_from), to(_to), count_vehicles(10) {}
+		Demo(models::GeoPoint c, double r): center(c), radius(r), count_vehicles(10) {}
 
 		//if user doesn't select special vehicle
 		std::vector<models::Vehicle> generateAllVehicles();
@@ -18,14 +19,27 @@ namespace components {
 		//if user select special vehicle
 		std::vector<models::Vehicle> generateSpecVehicles(long long vehicle_id);
 
-		models::Vehicle getNearestVehicle();
+		//here should be also type of vehicle, 
+		//returns a driver with nearest point and needed model  
+		models::Vehicle getNearestVehicle(models::GeoPoint c);
+
+		void resetArea(models::GeoPoint c, double r);
+
+		//this method should be restarted every n miliseconds 
+		//and update position of each vehicle in orders
+		std::vector<models::Order> updatePositions();
+
+		void addNewOrder(models::Order order);
+		void cancelOrder(models::Order order);
 
 	private:
 		int count_vehicles;
 
-		models::GeoPoint from, to; 
+		//these two parameteres for limit area on map
+		models::GeoPoint center;
+		double radius; 
 
-		models::Vehicle cur_vehicle;
+		std::vector<models::Order> orders;
 
 		DbManager &db;
 	};
