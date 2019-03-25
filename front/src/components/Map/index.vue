b5e2e2edc05ce255352dedc85090bad686f73552
Alexandra Kruchinina <krulex98@gmail.com>
2019-03-25 14:38:18 +0300

    front was moved to another repository.

--- front/src/components/Map/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./Map.html"></template>
-<script src="./Map.js"></script>
-<style src="./Map.scss" scoped lang="scss"></style>
-
--- front/src/components/Order/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./Order.html"></template>
-<script src="./Order.js"></script>
-<style src="./Order.scss" scoped lang="scss"></style>
-
