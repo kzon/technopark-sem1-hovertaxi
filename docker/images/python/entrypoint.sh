c0c0f8137c6d1d7eaff7d8fec1c8051d25cfe61b
anton <anton.cobzew@yandex.ru>
2019-03-29 17:04:42 +0300

    Cython build inside Docker

--- docker/images/python/entrypoint.sh
+++ docker/images/python/entrypoint.sh
@@ -4,6 +4,13 @@ git config --global user.name "Anton Kobzev"
 git config --global user.email "anton.cobzew@yandex.ru"
 cmake .
 
+make aircraft
+mv src/controllers/aircraft/aircraft.so aircraft.so
+
+make demo
+mv src/controllers/demo/demo.so demo.so
+
+
 # Start server
 cd /home/hovertaxi
 python src/app.py
