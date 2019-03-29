41812a956555f9e4e097634f5262166f6bd40abd
anton <anton.cobzew@yandex.ru>
2019-03-29 16:45:34 +0300

    Add mongo

--- docker/images/python/entrypoint.sh
+++ docker/images/python/entrypoint.sh
@@ -1,3 +1,9 @@
+# Build Cython modules
+cd /home/hovertaxi/src/extern
+git config --global user.name "Anton Kobzev"
+git config --global user.email "anton.cobzew@yandex.ru"
+cmake .
+
 # Start server
 cd /home/hovertaxi
 python src/app.py
