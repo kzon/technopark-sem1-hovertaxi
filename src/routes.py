e57902107ade9072d012ede211e7231bf6428cd5
anton <anton.cobzew@yandex.ru>
2019-04-03 10:45:30 +0300

    API routes prefix

--- src/routes.py
+++ src/routes.py
@@ -3,8 +3,8 @@ from controllers import *
 routes = [
     (r"/", main.MainController.IndexHandler),
 
-    (r"/order/preOrder", order.OrderController.PreOrderHandler),
+    (r"/api/order/preOrder", order.OrderController.PreOrderHandler),
 
-    (r"/aircraft/loadInCircle", aircraft.AircraftController.LoadInCircleHandler),
-    (r"/aircraft/loadAircraftClasses", aircraft.AircraftController.LoadAircraftClassesHandler),
+    (r"/api/aircraft/loadInCircle", aircraft.AircraftController.LoadInCircleHandler),
+    (r"/api/aircraft/loadAircraftClasses", aircraft.AircraftController.LoadAircraftClassesHandler),
 ]
