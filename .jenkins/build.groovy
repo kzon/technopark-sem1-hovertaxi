af3a74edef77581f75c3568b24fe6f0a0f6901a4
krulex98 <krulex98@gmail.com>
2019-04-22 00:26:29 +0300

    add jenkins

---
+++ .jenkins/build.groovy
@@ -0,0 +1,16 @@
+#!groovy
+
+node {
+
+    try{
+        runTestsStage()
+        currentBuild.result = 'SUCCESS'
+    } catch(err){
+        currentBuild.result = 'FAILED'
+    }
+}
+
+def runTestsStage(){
+    stage 'Tests'
+    sh 'sh ../docker/bin/tests.sh'
+}
