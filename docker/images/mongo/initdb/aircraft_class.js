5b65782e28891160a5bfedfc8fa14e840f57b64e
anton <anton.cobzew@yandex.ru>
2019-04-30 12:31:04 +0300

    Add initial aircraft classes

---
+++ docker/images/mongo/initdb/aircraft_class.js
@@ -0,0 +1,5 @@
+db.aircraft_class.insert([
+    {name: "Helicopter", max_range: NumberInt(100), min_range: NumberInt(2)},
+    {name: "Multicopter", max_range: NumberInt(50), min_range: NumberInt(1)},
+    {name: "Hoverbike", max_range: NumberInt(20), min_range: NumberInt(0)},
+]);
