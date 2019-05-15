2cbcdc3d34ac3fa56db1e065ccaf834771320e1c
anton <anton.cobzew@yandex.ru>
2019-05-15 16:40:30 +0300

    Fix user id cookie set

--- src/web.py
+++ src/web.py
@@ -5,20 +5,20 @@ import tornado.web
 import tornado.httputil
 import models
 
+
 class BaseJSONHandler(tornado.web.RequestHandler):
     def set_default_headers(self, *args, **kwargs):
         self.set_header("Content-Type", "application/json")
-
-    def options(self):
-        pass
 
 
 class BaseHandlerWithExternModule(BaseJSONHandler, ABC):
     def __init__(self, application, request: tornado.httputil.HTTPServerRequest, **kwargs):
         super().__init__(application, request, **kwargs)
         if 'user_id' not in self.cookies:
-            self.set_secure_cookie('user_id', str(uuid.uuid4()))
-        user_id = self.get_secure_cookie('user_id').decode()
+            user_id = str(uuid.uuid4())
+            self.set_secure_cookie('user_id', user_id)
+        else:
+            user_id = self.get_secure_cookie('user_id').decode()
         self.extern_wrapper = self.get_extern_module().get_wrapper(models.Context(user_id))
 
     def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
