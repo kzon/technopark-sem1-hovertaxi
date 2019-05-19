dda293a773e44a4df989fedee5a504d377022dc2
krulex98 <krulex98@gmail.com>
2019-05-19 03:13:51 +0300

    added method "get cancel price" for existing order

--- src/extern/src/components/order/price_service/price_service.cpp
+++ src/extern/src/components/order/price_service/price_service.cpp
@@ -6,8 +6,31 @@ int PriceService::GetPrice(const Order &order) {
   return 0;
 }
 
+// cancelPrice = (current distance / total distance  * price) / 2
 int PriceService::GetCancelPrice(const Order &order) {
-  return 0;
+
+  DataManager &data_manager_ = DataManager::GetInstance();
+
+  Optional<Pad> from_pad_result = data_manager_.LoadPadById(order.from_pad_id),
+      to_pad_result = data_manager_.LoadPadById(order.to_pad_id);
+  if (!from_pad_result || !to_pad_result)
+    throw std::bad_exception();
+  Pad from_pad = from_pad_result.value(),
+      to_pad = to_pad_result.value();
+
+  Optional<Aircraft> aircraft_result  = data_manager_.LoadNearestFreeAircraft(from_pad.position, order.aircraft_class_id);
+  if(!aircraft_result)
+    throw std::bad_exception();
+  Aircraft aircraft = aircraft_result.value();
+
+  GeoPoint from = from_pad.position;
+  GeoPoint current = aircraft.position;
+
+  double current_distance = Geo::DistanceEarth(from, current);
+  double total_distance = Geo::DistanceEarth(from_pad.position, to_pad.position);
+  int cancelPrice = (int) ( current_distance / total_distance * order.price / 2 );
+
+  return cancelPrice;
 }
 
 int PriceService::GetTimeFlight(const GeoPoint &p1, const GeoPoint &p2, const AircraftModel &model) {
@@ -19,6 +42,7 @@ int PriceService::GetTimeFlight(const GeoPoint &p1, const GeoPoint &p2, const Ai
   return time;
 }
 
+// price = ( (s / v) * 60 ) * coef(aircraft_class)
 int PriceService::GetPrice(const GeoPoint &p1, const GeoPoint &p2, const AircraftModel &model) {
 
   DataManager &data_manager_ = DataManager::GetInstance();
