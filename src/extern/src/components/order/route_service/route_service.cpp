fcdfaf20b3ced3cafd807f131c42a77a56f1e1d8
krulex98 <krulex98@gmail.com>
2019-05-20 11:05:51 +0300

    calc time to arrival

--- src/extern/src/components/order/route_service/route_service.cpp
+++ src/extern/src/components/order/route_service/route_service.cpp
@@ -13,12 +13,16 @@ std::vector<Route> RouteService::GetCrossingRoutes(const Route &route) {
 int RouteService::GetTimeToArrival(const Order &order) const {
   if (order.status == Order::STATUS_AIRCRAFT_ASSIGNED) {
     Aircraft assigned_aircraft = data_manager_.LoadAircraftById(order.assigned_aircraft_id).value();
+    AircraftModel model = data_manager_.LoadAircraftModelById(assigned_aircraft.model_id).value();
     Pad from_pad = data_manager_.LoadPadById(order.from_pad_id).value();
-    return 0;
+    int time = GetTimeFlight(assigned_aircraft.position, from_pad.position, model);
+    return time;
   } else if (order.status == Order::STATUS_ON_FLY) {
     Aircraft assigned_aircraft = data_manager_.LoadAircraftById(order.assigned_aircraft_id).value();
+    AircraftModel model = data_manager_.LoadAircraftModelById(assigned_aircraft.model_id).value();
     Pad to_pad = data_manager_.LoadPadById(order.to_pad_id).value();
-    return 0;
+    int time = GetTimeFlight(assigned_aircraft.position, to_pad.position, model);
+    return time;
   }
   return 0;
 }
